caaa8a59121f5f55e8b8eed3b30ddd72590a4af4
George Kiknadze <zinguru@gmail.com>
2026-02-15 11:13:46 +0300

    removed test files

--- nvim/.config/nnnvim/lua/config/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'sainnhe/everforest',
-  priority = 1000,
-  config = function()
-    vim.g.everforest_enable_italic = false
-    vim.g.everforest_background = 'hard'
-    vim.cmd.colorscheme 'everforest'
-  end,
-}
